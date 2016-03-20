085284a866f4d2c6a217607863e8bee6ee481c31
loquox <crloquox@gmail.com>
2016-03-20 18:09:47 +0100

    formulario insertar

--- app/templates/formInsertar.php
+++ app/templates/formInsertar.php
@@ -14,8 +14,58 @@
         <th>Proteina (g)</th>
         <th>H. de carbono</th>
         <th>Fibra (g)</th>
-        <th>d       </th>
+        <th>Grasa total (g)</th>
     </tr>
+    <tr>
+        <td>
+            <input type="text" name="nombre" value="<?php echo 
+            $params['nombre']?>"/>
+        </td>
+        <td>
+            <input type="text" name="energia" value="<?php echo
+            $params['energia']?>"/>
+        </td>
+        <td>
+            <input type="text" name="proteina" value="<?php echo
+            $params['proteina']?>" />
+        </td>
+        <td>
+            <input type="text" name="hc" value="<?php echo 
+            $params['hc']?> " />
+        </td>
+        <td>
+            <input type="text" name="fibra" value="<?php echo
+            $params['fibra']?>" />
+        </td>
+        <td>
+            <input type="text" mame="grasa" value="<?php echo
+            $params['grasa']?>" />
+        </td>
+    </tr>
+    
+    </table>
+    <input type="submit" value="insertar" name="insertar" />
+</form>
+
+<?php $contenido = ob_get_clean() ?>
+<?php include 'layout.php' ?>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+            
+            
+            
+                
+                
+    
+    
 </table>
 
 
